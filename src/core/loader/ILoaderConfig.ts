6c8801d5bb6f1bea14fccadd925fac117fb0db69
cdriscol <chris@driscolsoftware.com>
2020-05-19 16:24:50 -0600

    IDecisionRequest can now be typed with custom context

--- src/core/loader/ILoaderConfig.ts
+++ src/core/loader/ILoaderConfig.ts
@@ -1,12 +1,12 @@
-import { IDecisionRequest } from "../IDecisionRequest";
+import { IDecisionRequest, IDecisionContext } from "../IDecisionRequest";
 
-export interface ILoaderConfig<U, R> {
+export interface ILoaderConfig<U, R, C extends IDecisionContext = IDecisionContext> {
     // unique name used to store data from resolve
     name: string;
 
     // returns any object which will be stored in the context
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    resolve(R: IDecisionRequest<U, R>): Promise<any>;
+    resolve(R: IDecisionRequest<U, R, C>): Promise<any>;
 
     // loaders needed before this loader to resolve data (circular loaders not allowed)
     loaders?: ILoaderConfig<U, R>[];
