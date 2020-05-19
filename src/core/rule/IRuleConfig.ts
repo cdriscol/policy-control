4eae43fd3e0323db694c25cf0c62efcff54ad39d
cdriscol <chris@driscolsoftware.com>
2020-05-19 15:40:41 -0600

    Rule result is a maybe promise

--- src/core/rule/IRuleConfig.ts
+++ src/core/rule/IRuleConfig.ts
@@ -1,11 +1,13 @@
 import { ILoaderConfig } from "../loader";
 import { IDecisionRequest } from "../IDecisionRequest";
 
+type RuleResult = boolean | undefined | null;
+
 export interface IRuleConfig<U, R> {
     // name of rule, mostly used for debugging
     name: string;
     // loaders needed before rule can be evaluated
     loaders?: ILoaderConfig<U, R>[];
     // evaluate rule returning TRUE if rule passes, FALSE if rule fails, or null/undefined if rule cannot determine a result
-    evaluate(R: IDecisionRequest<U, R>): Promise<boolean | undefined | null>;
+    evaluate(R: IDecisionRequest<U, R>): RuleResult | Promise<RuleResult>;
 }
